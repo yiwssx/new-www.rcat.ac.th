4a5834885dee32bc1a1d3a32d15f469c256f2194
yiwssx <yiwssx@users.noreply.github.com>
2026-05-19 20:23:47 +0700

    Add TypeScript typing for Vite environment variables.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,10 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_CMS_SITE_NAME?: string;
   readonly VITE_GOOGLE_APPS_SCRIPT_URL?: string;
-  readonly VITE_PUBLIC_ANALYTICS_STRATEGY?: "gtm" | "gtag" | "both";
   readonly VITE_PUBLIC_SITE_URL?: string;
+  readonly VITE_CMS_SITE_NAME?: string;
+  readonly VITE_PUBLIC_ANALYTICS_STRATEGY?: "gtm" | "gtag" | "both";
 }
 
 interface ImportMeta {
