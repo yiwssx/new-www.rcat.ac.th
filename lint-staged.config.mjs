91a70ab37a41c963251960b21374e3add5e03120
yiwssx <yiwssx@users.noreply.github.com>
2026-05-19 21:29:10 +0700

    Add a simple lint-staged configuration.

--- lint-staged.config.mjs
+++
@@ -1,6 +0,0 @@
-export default {
-  // Lint and format JS/TS files
-  "**/*.{js,ts,tsx,mjs}": ["eslint --fix", "prettier --write"],
-  // Format JSON, Markdown, CSS, HTML, YAML
-  "**/*.{json,md,css,html,yaml,yml}": ["prettier --write"]
-};
