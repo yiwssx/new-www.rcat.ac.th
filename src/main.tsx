4c319b8d12732941de366567c6f906167e6820f6
Supharoek Sudadet <48784420+yiwssx@users.noreply.github.com>
2026-08-03 06:40:17 +0700

    refactor(ssr): create browser runtime instances at entrypoint

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { projectSettings } from "./config/projectSettings";
+import { createAppQueryClient } from "./queryClient";
+import { createAppRouter } from "./routes";
 import "./styles.css";
 import { installBrowserErrorFilters } from "./utils/browserErrorFilters";
 
@@ -9,8 +11,11 @@ installBrowserErrorFilters();
 document.documentElement.lang = projectSettings.site.language;
 document.title = projectSettings.site.name;
 
+const queryClient = createAppQueryClient();
+const router = createAppRouter();
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <App queryClient={queryClient} router={router} />
   </React.StrictMode>
 );
