5c0932c60045a0b754ff0cef3850b2a037338511
Supharoek Sudadet <48784420+yiwssx@users.noreply.github.com>
2026-08-03 09:20:15 +0700

    style(ssr): format home snapshot hook

--- src/public/hooks/usePublicHomeSnapshot.ts
+++ src/public/hooks/usePublicHomeSnapshot.ts
@@ -1,10 +1,6 @@
 import { useMemo } from "react";
 import { useQuery } from "@tanstack/react-query";
-import {
-  getPublicHomeCache,
-  PUBLIC_HOME_CACHE_TTL_MS,
-  publicHomeQueryOptions
-} from "../../features/public-home";
+import { getPublicHomeCache, PUBLIC_HOME_CACHE_TTL_MS, publicHomeQueryOptions } from "../../features/public-home";
 import { isPublicQueryCacheFresh } from "../../features/public-read/queryPolicy";
 
 export function usePublicHomeSnapshot() {
