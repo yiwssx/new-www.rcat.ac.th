87ede7867acfde4e4025893720bb0be599b362f0
Supharoek Sudadet <48784420+yiwssx@users.noreply.github.com>
2026-08-04 13:37:30 +0700

    feat(ssr): hydrate production SSR at document root

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -5,28 +5,38 @@ import App from "./App";
 import AppProviders from "./AppProviders";
 import { projectSettings } from "./config/projectSettings";
 import { createAppRuntime } from "./runtime";
+import { SSR_DOCUMENT_MARKER_ATTRIBUTE, SSR_DOCUMENT_MARKER_VALUE } from "./ssrAssets";
 import { installBrowserErrorFilters } from "./utils/browserErrorFilters";
 
 export function shouldHydrateClientApp(rootElement: HTMLElement) {
   return rootElement.hasChildNodes();
 }
 
+export function shouldHydrateSsrDocument(documentElement: HTMLElement = document.documentElement) {
+  return documentElement.getAttribute(SSR_DOCUMENT_MARKER_ATTRIBUTE) === SSR_DOCUMENT_MARKER_VALUE;
+}
+
 export function mountClientApp(rootElement: HTMLElement) {
   installBrowserErrorFilters();
   document.documentElement.lang = projectSettings.site.language;
 
-  const runtime = createAppRuntime();
+  const documentMode = shouldHydrateSsrDocument();
+  const runtime = createAppRuntime({ documentMode });
+  const routerClient = (
+    <React.StrictMode>
+      <AppProviders emotionCache={runtime.emotionCache} queryClient={runtime.queryClient}>
+        <RouterClient router={runtime.router} />
+      </AppProviders>
+    </React.StrictMode>
+  );
+
+  if (documentMode) {
+    hydrateRoot(document, routerClient);
+    return runtime;
+  }
 
   if (shouldHydrateClientApp(rootElement)) {
-    hydrateRoot(
-      rootElement,
-      <React.StrictMode>
-        <AppProviders emotionCache={runtime.emotionCache} queryClient={runtime.queryClient}>
-          <RouterClient router={runtime.router} />
-        </AppProviders>
-      </React.StrictMode>
-    );
-
+    hydrateRoot(rootElement, routerClient);
     return runtime;
   }
 
