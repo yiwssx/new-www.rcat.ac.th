9a11acf1e0c5213bad98ba529e8cfa9ef977d5da
Supharoek Sudadet <48784420+yiwssx@users.noreply.github.com>
2026-08-15 18:45:18 +0700

    refactor(p5): make public shell the only shell owner

--- src/features/public-shell/api.ts
+++ src/features/public-shell/api.ts
@@ -1,26 +1,7 @@
-import {
-  getPublicHomeSnapshotFromCloudflare,
-  getPublicShellSnapshotFromCloudflare,
-  isCloudflarePublicApiNotFoundError
-} from "../public-read/cloudflareApi";
+import { getPublicShellSnapshotFromCloudflare } from "../public-read/cloudflareApi";
 import type { PublicReadRequestOptions } from "../public-read/request";
 import type { PublicShellSnapshot } from "../../types";
 
-export async function getPublicShellSnapshot(options: PublicReadRequestOptions = {}): Promise<PublicShellSnapshot> {
-  try {
-    return await getPublicShellSnapshotFromCloudflare(options);
-  } catch (error) {
-    if (!isCloudflarePublicApiNotFoundError(error)) {
-      throw error;
-    }
-
-    const home = await getPublicHomeSnapshotFromCloudflare(options);
-    return {
-      siteSettings: home.siteSettings,
-      homepageSettings: home.homepageSettings,
-      displaySettings: home.displaySettings,
-      menu: home.menu,
-      generatedAt: home.generatedAt
-    };
-  }
+export function getPublicShellSnapshot(options: PublicReadRequestOptions = {}): Promise<PublicShellSnapshot> {
+  return getPublicShellSnapshotFromCloudflare(options);
 }
