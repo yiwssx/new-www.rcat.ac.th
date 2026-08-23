52353c11d580bed49520ed4cd99aa4e669ef57c7
Supharoek Sudadet <48784420+yiwssx@users.noreply.github.com>
2026-08-23 12:51:09 +0700

    feat(admin-ux): route menu through workflow guide

---
+++ src/admin/pages/MenuManagementWorkflowPage.tsx
@@ -0,0 +1,11 @@
+import MenuManagementWorkflowGuide from "../components/MenuManagementWorkflowGuide";
+import MenuPage from "./MenuPage";
+
+export default function MenuManagementWorkflowPage() {
+  return (
+    <>
+      <MenuManagementWorkflowGuide />
+      <MenuPage />
+    </>
+  );
+}
