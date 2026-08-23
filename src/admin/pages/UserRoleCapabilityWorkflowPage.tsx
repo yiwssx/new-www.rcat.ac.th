bb0b70383ed41203d16ed2d0523dc0b656a6da9e
Supharoek Sudadet <48784420+yiwssx@users.noreply.github.com>
2026-08-23 21:07:27 +0700

    feat(admin-ux): route users through role capability workflow

---
+++ src/admin/pages/UserRoleCapabilityWorkflowPage.tsx
@@ -0,0 +1,11 @@
+import UserRoleCapabilityWorkflowGuide from "../components/UserRoleCapabilityWorkflowGuide";
+import UsersPage from "./UsersPage";
+
+export default function UserRoleCapabilityWorkflowPage() {
+  return (
+    <>
+      <UserRoleCapabilityWorkflowGuide />
+      <UsersPage />
+    </>
+  );
+}
