fa94eacfb7f746e7b87acc998ef23173f06a16cd
Supharoek Sudadet <48784420+yiwssx@users.noreply.github.com>
2026-09-02 19:21:56 +0700

    feat(media): export Facebook thumbnail progress types

--- src/features/cms-media/index.ts
+++ src/features/cms-media/index.ts
@@ -1,5 +1,13 @@
 export { deleteMediaAsset, importFacebookThumbnailAsset, saveMediaAsset, uploadMediaAsset } from "./api";
 export { mergeBridgeMediaAssets } from "./bridgeCache";
 export { MAX_MEDIA_UPLOAD_BYTES } from "./mediaBridgeClient";
-export type { FacebookThumbnailImportInput, MediaAssetInput, MediaUploadOptions, MediaUploadProgress } from "./api";
+export type {
+  FacebookThumbnailImportInput,
+  FacebookThumbnailImportOptions,
+  FacebookThumbnailProgress,
+  FacebookThumbnailProgressPhase,
+  MediaAssetInput,
+  MediaUploadOptions,
+  MediaUploadProgress
+} from "./api";
 export type { MediaAsset, MediaType } from "./types";
