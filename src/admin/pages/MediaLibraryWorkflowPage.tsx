5d1e9acdc037f24f7b9ed5037624f4965177809f
Supharoek Sudadet <48784420+yiwssx@users.noreply.github.com>
2026-09-23 15:27:35 +0700

    feat(cms): surface media usage governance in the library

--- src/admin/pages/MediaLibraryWorkflowPage.tsx
+++ src/admin/pages/MediaLibraryWorkflowPage.tsx
@@ -1,11 +1,13 @@
 import Stack from "@mui/material/Stack";
 import MediaLibraryWorkflowGuide from "../components/MediaLibraryWorkflowGuide";
+import MediaUsageGovernancePanel from "../components/MediaUsageGovernancePanel";
 import MediaPage from "./MediaPage";
 
 export default function MediaLibraryWorkflowPage() {
   return (
     <Stack spacing={3}>
       <MediaLibraryWorkflowGuide />
+      <MediaUsageGovernancePanel />
       <MediaPage />
     </Stack>
   );
