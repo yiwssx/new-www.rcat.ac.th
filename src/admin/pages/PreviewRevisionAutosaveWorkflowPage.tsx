31d63f75d4b321ef4da043b04249a0dfbfa2d737
Supharoek Sudadet <48784420+yiwssx@users.noreply.github.com>
2026-09-26 16:40:13 +0700

    feat(cms): surface gap closure content tools

--- src/admin/pages/PreviewRevisionAutosaveWorkflowPage.tsx
+++ src/admin/pages/PreviewRevisionAutosaveWorkflowPage.tsx
@@ -1,8 +1,11 @@
 import Stack from "@mui/material/Stack";
 import ContentHealthDashboard from "../components/ContentHealthDashboard";
 import ContentLifecycleGovernancePanel from "../components/ContentLifecycleGovernancePanel";
+import ContentRedirectManagerPanel from "../components/ContentRedirectManagerPanel";
 import EditorialWorkflowPanel from "../components/EditorialWorkflowPanel";
 import PreviewRevisionAutosaveWorkflowGuide from "../components/PreviewRevisionAutosaveWorkflowGuide";
+import RevisionComparePanel from "../components/RevisionComparePanel";
+import TaxonomyManagerPanel from "../components/TaxonomyManagerPanel";
 import ContentPage from "./ContentPage";
 
 export default function PreviewRevisionAutosaveWorkflowPage() {
@@ -12,6 +15,9 @@ export default function PreviewRevisionAutosaveWorkflowPage() {
       <EditorialWorkflowPanel />
       <ContentHealthDashboard />
       <ContentLifecycleGovernancePanel />
+      <RevisionComparePanel />
+      <TaxonomyManagerPanel />
+      <ContentRedirectManagerPanel />
       <ContentPage />
     </Stack>
   );
